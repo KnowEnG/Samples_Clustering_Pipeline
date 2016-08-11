d4de3c2f55d2c7d3e96ff8d4fc1d4fc1b8709bcc
Daniel Lanier <lanier4@mntl-dhcp108.mntl.illinois.edu>
2016-08-11 16:59:43 -0500

    retailored to work with actual knpackage.toolbox.py

--- test/unit/toolbox_test.py
+++ test/unit/toolbox_test.py
@@ -3,13 +3,29 @@
 
 import unittest
 import numpy as np
-import numpy.linalg as lpac
+#import numpy.linalg as LA
 import scipy.sparse as spar
 
 # file version to test
 import knpackage.toolbox as keg
 
 class toolbox_test(unittest.TestCase):
+    
+    def get_run_parameters(self):
+        run_parameters = {'k':3,'number_of_iteriations_in_rwr':100,
+                          'obj_fcn_chk_freq':50,
+                          'it_max':10000,
+                          'h_clust_eq_limit':100,
+                          'restart_tolerance':0.0001,
+                          'lmbda':1400,
+                          'percent_sample':0.8,
+                          'number_of_bootstraps':3,
+                          'display_clusters':1,
+                          'restart_probability':0.7,
+                          'verbose':1,
+                          'use_now_name':1000000}
+
+        return run_parameters
     
     def testQN(self):
         a = np.array([[7.0, 5.0],[3.0, 1.0],[1.0,7.0]])
@@ -18,42 +34,19 @@ class toolbox_test(unittest.TestCase):
         
         self.assertEqual(sum(sum(qn1 != aQN)), 0, 'Quantile Norm 1 Not Equal')
         
-    def testQN_T(self):
-        a = np.array([[7.0, 5.0],[3.0, 1.0],[1.0,7.0]])
-        aQN = np.array([[7.0, 4.0],[4.0,1.0],[1.0,7.0]])
-        qn1 = keg.get_quantile_norm_matrix(a.T)
-        qn1 = qn1.T
-        
-        self.assertEqual(sum(sum(qn1 != aQN)), 0, 'Quantile Norm 1 Not Equal')
-        
     def testRWR(self):
-
-        alpha = 1.0
-        maxIt = 100
-        tol = 1e-8
-
-        F0 = np.array([1.0, 0.0])
+        run_parameters = self.get_run_parameters()
+        run_parameters['restart_probability'] = 1
+        run_parameters['restart_tolerance'] = 1e-12
+        F0 = np.eye(2)
         A = spar.csr_matrix(( (np.eye(2) + np.ones(2)) / 3) )
-        A = spar.csr_matrix(( (np.eye(2) + np.ones(2)) / 3) )
+        # A = (np.eye(2) + np.ones((2,2))) / 3
         
-        F_exact = np.array([0.5, 0.5])
-        F_calculated = keg.smooth_matrix_with_rwr(F0, A, alpha, maxIt, tol)
-        self.assertLessEqual( lpac.norm(F_exact - F_calculated), tol)
+        F_exact = np.ones((2, 2)) * 0.5
+        F_calculated, steps = keg.smooth_matrix_with_rwr(F0, A, run_parameters)
+        T = (np.abs(F_exact - F_calculated))
         
-    def testRWR_2(self):
-
-        alpha = 0.5
-        maxIt = 100
-        tol = 1e-8
-
-        F0 = np.array([1.0, 0.0])
-        A = spar.csr_matrix(((np.eye(2) + np.ones(2)) / 3))
-        #A = spar.csr_matrix(A)
-        
-        F_exact = np.array([0.8, 0.2])
-        F_calculated = keg.smooth_matrix_with_rwr(F0, A, alpha, maxIt, tol)
-        self.assertLessEqual( lpac.norm(F_exact - F_calculated), tol)     
-        
+        self.assertAlmostEqual(T.sum(), 0)
         
 
 def suite():
