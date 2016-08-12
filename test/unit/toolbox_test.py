93c6fdbc248003d412260cc6d2ff163c2a7d301a
Daniel Lanier <lanier4@mntl-dhcp108.mntl.illinois.edu>
2016-08-12 15:13:30 -0500

    get_timestamp unit test added

--- test/unit/toolbox_test.py
+++ test/unit/toolbox_test.py
@@ -186,6 +186,19 @@ class toolbox_test(unittest.TestCase):
         kn.remove_dir(ndr)
         self.assertFalse(os.path.exists(ndr), msg='remove_dir function exception')
         
+    def test_get_timestamp(self):
+        """ assert that the default size of the timestamp string is 16 chars and
+            that sequential calls produce differnt results
+        """
+        n_default_chars = 16
+        stamp_time = 1e6
+        tstr = kn.get_timestamp(stamp_time)
+        tstr2 = kn.get_timestamp()
+        
+        self.assertEqual(len(tstr), n_default_chars, msg='string return size unexpected')
+        self.assertNotEqual(tstr, tstr2)
+        
+        
     """
 def get_timestamp(stamp_units=1e6):
     get a time stamp string - current time as integer string.
