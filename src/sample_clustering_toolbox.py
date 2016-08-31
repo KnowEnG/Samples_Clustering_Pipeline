9c36bcfa68b13a7d26cb20fa148c96aac81a09ef
Daniel Lanier <lanier4@mntl-dhcp108.mntl.illinois.edu>
2016-08-31 12:21:51 -0500

    spelling: parallel fixed

--- src/sample_clustering_toolbox.py
+++ src/sample_clustering_toolbox.py
@@ -58,12 +58,12 @@ def run_cc_nmf(run_parameters):
     spreadsheet_df = kn.get_spreadsheet_df(run_parameters['spreadsheet_name_full_path'])
     spreadsheet_mat = spreadsheet_df.as_matrix()
     spreadsheet_mat = kn.get_quantile_norm_matrix(spreadsheet_mat)
-    if int(run_parameters['use_paralell_processing']) != 0:
+    if int(run_parameters['use_parallel_processing']) != 0:
         # Number of processes to be executed in parallel
         number_of_cpus = multiprocessing.cpu_count()
         print("Using parallelism {}".format(number_of_cpus))
 
-        find_and_save_nmf_clusters_paralell(spreadsheet_mat, run_parameters, number_of_cpus)
+        find_and_save_nmf_clusters_parallel(spreadsheet_mat, run_parameters, number_of_cpus)
     else:
         find_and_save_nmf_clusters_serial(spreadsheet_mat, run_parameters)
 
@@ -161,12 +161,12 @@ def run_cc_net_nmf(run_parameters):
     spreadsheet_df = kn.update_spreadsheet_df(spreadsheet_df, unique_gene_names)
     spreadsheet_mat = spreadsheet_df.as_matrix()
     sample_names = spreadsheet_df.columns
-    if int(run_parameters['use_paralell_processing']) != 0:
+    if int(run_parameters['use_parallel_processing']) != 0:
         # Number of processes to be executed in parallel
         number_of_cpus = multiprocessing.cpu_count()
         print("Using parallelism {}".format(number_of_cpus))
 
-        find_and_save_net_nmf_clusters_paralell(network_mat, spreadsheet_mat, lap_diag, lap_pos,
+        find_and_save_net_nmf_clusters_parallel(network_mat, spreadsheet_mat, lap_diag, lap_pos,
                                                 run_parameters, number_of_cpus)
     else:
         find_and_save_net_nmf_clusters_serial(network_mat, spreadsheet_mat, lap_diag, lap_pos, run_parameters)
@@ -230,7 +230,7 @@ def find_and_save_net_nmf_clusters_serial(network_mat, spreadsheet_mat, lap_dag,
     for sample in range(0, number_of_bootstraps):
         exec_net_nmf_clusters_worker(network_mat, spreadsheet_mat, lap_dag, lap_val, run_parameters, sample)
 
-def find_and_save_net_nmf_clusters_paralell(network_mat, spreadsheet_mat, lap_dag, lap_val, run_parameters, number_of_cpus):
+def find_and_save_net_nmf_clusters_parallel(network_mat, spreadsheet_mat, lap_dag, lap_val, run_parameters, number_of_cpus):
     """ central loop: compute components for the consensus matrix from the input
         network and spreadsheet matrices and save them to temp files.
 
@@ -290,7 +290,7 @@ def find_and_save_nmf_clusters_serial(spreadsheet_mat, run_parameters):
     for sample in range(0, number_of_bootstraps):
         exec_nmf_clusters_worker(spreadsheet_mat, run_parameters, sample)
 
-def find_and_save_nmf_clusters_paralell(spreadsheet_mat, run_parameters, number_of_cpus):
+def find_and_save_nmf_clusters_parallel(spreadsheet_mat, run_parameters, number_of_cpus):
     """ central loop: compute components for the consensus matrix by
         non-negative matrix factorization.
 
