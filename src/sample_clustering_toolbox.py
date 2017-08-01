ef33ddada195656f9e0c244a1b31c25c186286d9
Nahil Sobh <nahil.sobh@gmail.com>
2017-08-01 09:08:03 -0700

    count the unique elements of the labels list

--- src/sample_clustering_toolbox.py
+++ src/sample_clustering_toolbox.py
@@ -404,7 +404,7 @@ def save_consensus_clustering(consensus_matrix, sample_names, labels, run_parame
     out_df = pd.DataFrame(data=consensus_matrix, columns=sample_names, index=sample_names)
     out_df.to_csv(get_output_file_name(run_parameters, 'consensus_matrix', 'viz'), sep='\t')
 
-    n_labels = len(labels)
+    n_labels = len(set(labels))
     n_samples= len(sample_names)
 
     if (n_labels < 2) or (n_labels > n_samples-1):
